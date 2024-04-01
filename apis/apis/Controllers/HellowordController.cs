5313d53759accc3119aab160ddf9267a89c30036
phuslee24 <tranphu.thpthht@gmail.com>
2024-04-01 14:13:17 +0700

    update Hello Word!

---
+++ apis/apis/Controllers/HellowordController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HellowordController : ControllerBase
+    {
+        [HttpGet]
+        public string index()
+        {
+            return "Hello Word!";
+        }
+    }
+}
