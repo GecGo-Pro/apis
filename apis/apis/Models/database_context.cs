2b81d8d2fcb4485866f693a0b01e41d602566666
phuslee24 <tranphu.thpthht@gmail.com>
2024-04-04 02:22:10 +0700

    update databasecontext

---
+++ apis/apis/Models/database_context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace apis.Models
+{
+    public class database_context : DbContext
+    {
+        public database_context(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<customer> customers { get; set; }
+        public DbSet<dispatcher> dispatchers { get; set; }
+        public DbSet<driver> drivers { get; set; }
+        public DbSet<car> cars { get; set; }
+        public DbSet<dispatch_job> dispatch_Jobs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
